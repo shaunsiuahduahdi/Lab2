7238ba1412d726ce87b7ba8c399344703f162d14
shaun <shaungabriel.23@ichat.sp.edu.sg>
2024-05-02 15:02:02 +0800

    ex1return

--- bmi.py
+++ bmi.py
@@ -7,12 +7,14 @@ def calculate_bmi(height, weight):
 
 
     if (bmi < 18.5):
-        print ("Underweight")
+        print ("Underweight")  
+        return(-1)
 
     elif(18.5<bmi<25):
         print ("Acceptable")
-
+        return(0)
     else:
         print ("lose some weight")
+        return(1)
     
 calculate_bmi(weight=100, height=1.30)
